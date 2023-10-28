f02ab6b72a5c1797f39b97a46286f256240bbd16
Erick Nieto <ericknieto44@gmail.com>
2023-10-28 04:48:14 -0500

    feat: Enhance Auth Service, Store Boolean in Session
    In this commit, the authentication service has been improved with the following updates:
    - Enhanced features and functionality in the authentication service.
    - The user's authentication status is now stored in the session for persistence.

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -5,10 +5,13 @@ export class AuthService {
   private _isLogin: boolean = false;
 
   get isLogin(): boolean {
+    const login = sessionStorage.getItem('isLogin');
+    this._isLogin = login !== null ? login === 'true' : false;
     return this._isLogin;
   }
 
   set isLogin(isLogin: boolean) {
     this._isLogin = isLogin;
+    sessionStorage.setItem('isLogin', this._isLogin.toString());
   }
 }
